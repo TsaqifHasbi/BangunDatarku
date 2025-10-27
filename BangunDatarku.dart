dc44b3a2b32b25104dab40e02ca8c2ba1ee1c8fc
TsaqifHasbi <tsaqifhasbi1717@gmail.com>
2025-10-27 17:40:44 +0700

    Update README.md dengan dokumentasi lengkap BangunDatarku dan perubahan bangunDatarku

--- BangunDatarku.dart
+++ BangunDatarku.dart
@@ -58,6 +58,68 @@ class Segitiga {
   }
 }
 
+class Trapesium {
+  void hitungLuas() {
+    print("Masukkan panjang sisi a trapesium:");
+    double a = double.parse(stdin.readLineSync()!);
+    print("Masukkan panjang sisi b trapesium:");
+    double b = double.parse(stdin.readLineSync()!);
+    print("Masukkan tinggi trapesium:");
+    double tinggi = double.parse(stdin.readLineSync()!);
+    double luas = 0.5 * (a + b) * tinggi;
+    print("Luas Trapesium: $luas");
+  }
+
+  void hitungKeliling() {
+    print("Masukkan sisi a trapesium:");
+    double a = double.parse(stdin.readLineSync()!);
+    print("Masukkan sisi b trapesium:");
+    double b = double.parse(stdin.readLineSync()!);
+    print("Masukkan sisi c trapesium:");
+    double c = double.parse(stdin.readLineSync()!);
+    print("Masukkan sisi d trapesium:");
+    double d = double.parse(stdin.readLineSync()!);
+    double keliling = a + b + c + d;
+    print("Keliling Trapesium: $keliling");
+  }
+}
+
+class JajarGenjang {
+  void hitungLuas() {
+    print("Masukkan alas jajar genjang:");
+    double alas = double.parse(stdin.readLineSync()!);
+    print("Masukkan tinggi jajar genjang:");
+    double tinggi = double.parse(stdin.readLineSync()!);
+    double luas = alas * tinggi;
+    print("Luas Jajar Genjang: $luas");
+  }
+
+  void hitungKeliling() {
+    print("Masukkan sisi a jajar genjang:");
+    double a = double.parse(stdin.readLineSync()!);
+    print("Masukkan sisi b jajar genjang:");
+    double b = double.parse(stdin.readLineSync()!);
+    double keliling = 2 * (a + b);
+    print("Keliling Jajar Genjang: $keliling");
+  }
+}
+
+class Lingkaran {
+  void hitungLuas() {
+    print("Masukkan jari-jari lingkaran:");
+    double r = double.parse(stdin.readLineSync()!);
+    double luas = 3.14 * r * r;
+    print("Luas Lingkaran: $luas");
+  }
+
+  void hitungKeliling() {
+    print("Masukkan jari-jari lingkaran:");
+    double r = double.parse(stdin.readLineSync()!);
+    double keliling = 2 * 3.14 * r;
+    print("Keliling Lingkaran: $keliling");
+  }
+}
+
 void main() {
   print("======================================");
   print("    Selamat Datang di BangunDatarKu   ");
@@ -66,13 +128,16 @@ void main() {
   print("1. Persegi");
   print("2. Persegi Panjang");
   print("3. Segitiga");
-  print("Masukkan pilihan (1-3): ");
+  print("4. Trapesium");
+  print("5. Jajar Genjang");
+  print("6. Lingkaran");
+  stdout.write("Masukkan pilihan (1-6): ");
   String? bangunDatar = stdin.readLineSync();
 
   print("\nPilih operasi yang ingin dilakukan:");
   print("1. Hitung Luas");
   print("2. Hitung Keliling");
-  print("Masukkan pilihan (1-2): ");
+  stdout.write("Masukkan pilihan (1-2): ");
   String? operasi = stdin.readLineSync();
 
   switch (bangunDatar) {
@@ -102,6 +167,36 @@ void main() {
         segitiga.hitungLuas();
       } else if (operasi == '2') {
         segitiga.hitungKeliling();
+      } else {
+        print("Pilihan operasi tidak valid!");
+      }
+      break;
+    case '4':
+      Trapesium trapesium = Trapesium();
+      if (operasi == '1') {
+        trapesium.hitungLuas();
+      } else if (operasi == '2') {
+        trapesium.hitungKeliling();
+      } else {
+        print("Pilihan operasi tidak valid!");
+      }
+      break;
+    case '5':
+      JajarGenjang jajarGenjang = JajarGenjang();
+      if (operasi == '1') {
+        jajarGenjang.hitungLuas();
+      } else if (operasi == '2') {
+        jajarGenjang.hitungKeliling();
+      } else {
+        print("Pilihan operasi tidak valid!");
+      }
+      break;
+    case '6':
+      Lingkaran lingkaran = Lingkaran();
+      if (operasi == '1') {
+        lingkaran.hitungLuas();
+      } else if (operasi == '2') {
+        lingkaran.hitungKeliling();
       } else {
         print("Pilihan operasi tidak valid!");
       }
